cf4ea75f5432fad963f197a1a078f7c89045545c
donatomontrone <donato.montrone@outlook.com>
2022-12-15 17:02:49 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@
 // //Milestone 2
 // // Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
 // //Click sul contatto mostra la conversazione del contatto cliccato
-// Milestone 3
+// //Milestone 3
 // //Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
-// Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+// //Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 // Milestone 4
 // Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
 // Milestone 5 - opzionale
@@ -183,7 +183,7 @@ const { createApp } = Vue
                     }
                 ],
                 currentIndex : 0,
-                newMessage : ''
+                newMessage : '',
             }
         },
         methods : {
@@ -202,8 +202,8 @@ const { createApp } = Vue
             },
             setReply(index){
                 setTimeout(() => {
-                    this.contacts[index].messages.push({ date: '16:00:01', message: 'ok', status: 'received'});
+                    this.contacts[index].messages.push({ date: '16:00:01', message: 'Ok!', status: 'received'});
                 }, 1000);
-            }
+            },
         }
 }).mount('#app')
